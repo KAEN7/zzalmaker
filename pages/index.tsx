35f400bdf18b920f2342a66cd39ae46befc6ef8b
kaen <kusdsuna@naver.com>
2022-02-17 16:03:25 +0900

    Add: 라이브러리 타입스크립트 및 기본 설치     1. dom to image     2. file saver

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useRef } from "react";
 import styles from "../styles/Home.module.css";
+import domtoimage from "dom-to-image";
 import { saveAs } from "file-saver";
+import malddaggu from "../public/image/malddaggu.png";
 
 const Home: NextPage = () => {
+	const zzalRef = useRef();
+
+	// 컴포넌트 다운로드 함수
+	const onDownloadBtn = () => {
+		const zRef = zzalRef.current;
+
+		domtoimage.toBlob(zRef).then((blob) => {
+			saveAs(blob, "zzal.png");
+		});
+	};
+
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -13,7 +27,15 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<button onClick={() => console.log("짤 생성")}></button>
+			{/*  이미지 영역 */}
+			<section ref={zzalRef}>
+				<Image src={malddaggu} alt="말대꾸" />
+				<span>모코코가..</span>
+				<span>말대꾸?</span>
+			</section>
+
+			{/*  생성 버튼 */}
+			<button onClick={() => onDownloadBtn()}></button>
 		</div>
 	);
 };
